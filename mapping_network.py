373ca5101540844cff5c2d89c381753f4d96dd8e
mehranagh20 <mehranagh200@gmail.com>
2025-02-05 11:25:09 -0800

    no clamp

--- mapping_network.py
+++ mapping_network.py
@@ -92,7 +92,7 @@ class MappingNetowrk(nn.Module):
 
         mean, logstd = styles[-1].chunk(2, dim=1)
         # we can scale the logstd
-        std = torch.exp(logstd).clamp(max=2.0)
+        std = torch.exp(logstd)
         print('params', torch.mean(mean), torch.mean(std), torch.mean(logstd))
         return mean, std
 
